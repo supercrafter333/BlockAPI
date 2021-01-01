6840ab9024c902c1282104b296718c5769120a85
supercrafter333 <justcoolstuff99@gmail.com>
2021-01-01 14:33:47 +0100

    Add Offline Blocks

--- src/supercrafter333/BlockAPI/API/OffBlockAPI.php
+++ src/supercrafter333/BlockAPI/API/OffBlockAPI.php
@@ -8,12 +8,14 @@ use supercrafter333\BlockAPI\BlockAPILoader;
 
 class OffBlockAPI
 {
-
+    
     public $name;
+    protected $config;
 
     public function __construct(string $name)
     {
         $this->name = $name;
+        $this->config = new Config(BlockAPILoader::getInstance()->getDataFolder() . "players/" . $this->name . ".yml", Config::YAML);
     }
 
     public static function getExtraConfigurationManager(string $player): Config
@@ -43,5 +45,47 @@ class OffBlockAPI
             }
         }
         return true;
+    }
+    
+    public function setBlockTime(int $amount, string $timeformat)
+    {
+        $bantime = new DateTime('+' . $amount . ' ' . $timeformat);
+        $date = new DateTime("now");
+        if (file_exists(BlockAPILoader::getInstance()->getDataFolder() . "players/" . $this->name . ".yml")) {
+            $exitsdate = new DateTime($this->config->get("date"));
+            if ($date >= $exitsdate) {
+                BlockAPI::getUnblockManager($this->name)->unBlock();
+            } else {
+                $this->config->set("date", $bantime->format("Y-m-d H:i:s"));
+                $this->config->save();
+            }
+        }
+    }
+
+    public function setBlocker(string $name)
+    {
+        $this->config->set("blocker", $name);
+        $this->config->save();
+    }
+
+    public function setBlockReason(string $reason)
+    {
+        $this->config->set("reason", $reason);
+        $this->config->save();
+    }
+
+    public function getBlockReason()
+    {
+        return $this->config->get("reason");
+    }
+
+    public function getBlocker()
+    {
+        return $this->config->get("blocker");
+    }
+
+    public function getBlockTime()
+    {
+        return $this->config->get("date");
     }
 }
